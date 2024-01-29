4a14305ef6f479269f9f0571dac428ba7fddd459
YuraBarykin <yBarykin@gmail.com>
2024-01-29 12:13:32 +0400

    start progect react-shop

---
+++ src/components/app-component/App.tsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+const App = () => {
+  return (
+    <div>
+      Hello world! 
+    </div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.tsx'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './src',
+    open: true,
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: ['', '.ts', '.tsx', '.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './src/index.html'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    clean: true
+  },
+  optimization: {
+    runtimeChunk: 'single'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|jpef|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ],
+  },
+};
